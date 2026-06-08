91b295d4351d3ba77ad714e5a7be4baecbe0e1b0
kehud <ehudkakun@hotmail.com>
2026-06-08 08:18:43 +0300

    chore: add Firebase environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyBtuS1_ODSdtYisLf_27elxf2cjphfmwNc",
+    authDomain: "mindtrack-app-c00c1.firebaseapp.com",
+    projectId: "mindtrack-app-c00c1",
+    storageBucket: "mindtrack-app-c00c1.firebasestorage.app",
+    messagingSenderId: "941135851018",
+    appId: "1:941135851018:web:4378f435edc255a3da79f3",
+    measurementId: "G-MVC8ESZL21"
+  },
+};
